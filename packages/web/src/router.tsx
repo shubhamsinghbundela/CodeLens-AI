f055bd251653bc7f19e31adfaf9fb9814eed98bc
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-09-05 17:35:12 +0530

    tanstack query setup done

--- packages/web/src/router.tsx
+++ packages/web/src/router.tsx
@@ -1,12 +1,24 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 
 export function getRouter() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        staleTime: 1000 * 60 * 5,
+      },
+    },
+  });
   const router = createTanStackRouter({
     routeTree,
     scrollRestoration: true,
     defaultPreload: "intent",
     defaultPreloadStaleTime: 0,
+    Wrap: ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
   });
 
   return router;
